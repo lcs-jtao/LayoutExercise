0cacdf5a3895dd11bbac5d535949a6c41e1860f2
Joyce Tao <jtao@lcs.on.ca>
2021-10-27 11:59:18 -0400

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Joyce Tao on 2021-10-27.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
